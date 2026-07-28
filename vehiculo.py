a23531411485041015279dc9238ee09ca16bbea4
Mewnot-jar <martinardiles.a@gmail.com>
2026-07-27 21:27:38 -0400

    feat/clase-vehiculo-encapsulamiento

---
+++ vehiculo.py
@@ -0,0 +1,46 @@
+class Vehiculo:
+    def __init__(self, marca, modelo, precio_diario, capacidad_personas, tipo_traccion, rendimiento_kml):
+        #atributos publicos
+        self.marca = marca
+        self.modelo = modelo
+
+        #atributos privados
+        self.set_precio_diario(precio_diario)
+        self.__capacidad_personas = capacidad_personas
+        self.__tipo_traccion = tipo_traccion
+        self.__rendimiento_kml = rendimiento_kml
+
+    def get_precio_diario(self):
+        return self.__precio
+
+    def set_precio_diario(self, nuevo_precio):
+        if nuevo_precio <= 0:
+            print(f"Alerta: el precio ${nuevo_precio} no es valido. Se asignara la tarifa Base $10000")
+            self.__precio = 10000
+        else:
+            self.__precio = nuevo_precio
+
+    def get_capacidad_personas(self):
+        return self.__capacidad_personas
+
+    def get_tipo_traccion(self):
+        return self.__tipo_traccion
+
+    def get_rendimiento_kml(self):
+        return self.__rendimiento_kml
+
+    def calcular_cotizacion(self, dias):
+        return self.get_precio_diario * dias
+
+    def mostrar_ficha_tecnica(self):
+        print("------- FICHA TECNICA -------")
+        print(f"Marca:            {self.marca}")
+        print(f"Modelo:           {self.modelo}")
+        print(f"Capacidad:        {self.get_capacidad_personas()}")
+        print(f"Traccion:         {self.get_tipo_traccion()}")
+        print(f"Rendimiento:      {self.get_rendimiento_kml()}")
+        print(f"Precio por dia:   ${self.get_precio_diario()}")
+        print("-----------------------------")
+
+auto = Vehiculo("Toyota", "Corolla", -20000, 5, "Delantero", 14)
+auto.mostrar_ficha_tecnica()
