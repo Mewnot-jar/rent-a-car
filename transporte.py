89b2a8551a4fd9ec7602999999f4b442fce95ebf
Mewnot-jar <martinardiles.a@gmail.com>
2026-08-02 17:23:18 -0400

    refac/Transporte-creacion-de-catalogo-de-transporte

--- transporte.py
+++ transporte.py
@@ -1,27 +1,13 @@
 from vehiculo import Vehiculo
 
 class Transporte(Vehiculo):
-    def __init__(self, marca, modelo, precio_diario, capacidad_personas, tipo_traccion, terreno_ideal, rendimiento_kml):
-        super().__init__(marca, modelo, precio_diario, capacidad_personas, tipo_traccion, terreno_ideal, rendimiento_kml)
-
-        self.modelos = {
-            "Sprinter": {
-                "motor": "2.1L Diesel", 
-                "capacidad_personas": 15,
-                "traccion": "4x2",
-                "rendimiento": 9.5,
-                "precio_dia": 65000
-
-            },
-            "Crafter": {
-                "motor": "2.0L Diesel", 
-                "capacidad_personas": 20,
-                "traccion": "4x2",
-                "rendimiento": 10.0,
-                "precio_dia": 75000
-            
-            },
-        }
-
+    def __init__(self, marca, modelo, motor, precio_diario, capacidad_personas, tipo_traccion, terreno_ideal, rendimiento_kml):
+        super().__init__(marca, modelo, motor, precio_diario, capacidad_personas, tipo_traccion, terreno_ideal, rendimiento_kml)
+        
     def calcular_cotizacion(self, dias):
         return super().calcular_cotizacion(dias) + self.get_capacidad_personas()
+
+Transporte.catalogo = {
+    "1": Transporte("Mercedes-Benz", "Sprinter", "2.1L Turbodiesel", 65000, 15, "4x2", "Grupo", 9.5),
+    "2": Transporte("Volkswagen", "Crafter", "2.0L TDI", 75000, 20, "4x2", "Grupo", 10.0)
+}
