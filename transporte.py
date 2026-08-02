6b974e3fd836cf1c654772c647d8f6ca3177c9b4
Mewnot-jar <martinardiles.a@gmail.com>
2026-08-02 02:18:21 -0400

    feat/clase-hija-Transporte-modelos-base-y-metodo-calcular_cotizacion

---
+++ transporte.py
@@ -0,0 +1,27 @@
+from vehiculo import Vehiculo
+
+class Transporte(Vehiculo):
+    def __init__(self, marca, modelo, precio_diario, capacidad_personas, tipo_traccion, terreno_ideal, rendimiento_kml):
+        super().__init__(marca, modelo, precio_diario, capacidad_personas, tipo_traccion, terreno_ideal, rendimiento_kml)
+
+        self.modelos = {
+            "Sprinter": {
+                "motor": "2.1L Diesel", 
+                "capacidad_personas": 15,
+                "traccion": "4x2",
+                "rendimiento": 9.5,
+                "precio_dia": 65000
+
+            },
+            "Crafter": {
+                "motor": "2.0L Diesel", 
+                "capacidad_personas": 3,
+                "traccion": "4x2",
+                "rendimiento": 7.0,
+                "precio_dia": 75000
+            
+            },
+        }
+
+    def calcular_cotizacion(self, dias):
+        return super().calcular_cotizacion(dias) + self.get_capacidad_personas()
