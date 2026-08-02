cf3c75e2c3e443f98f5f277bde815896456d69ca
Mewnot-jar <martinardiles.a@gmail.com>
2026-08-02 17:40:46 -0400

    refac/main-modificacion-de-funcion-evaluar_opcion

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from camioneta import Camioneta
 from camion import Camion
 from transporte import Transporte
 
+flota = {
+    "1": AutoSuv.catalogo,
+    "2": Camioneta.catalogo,
+    "3": Camion.catalogo,
+    "4": Transporte.catalogo
+}
+
 def mostrar_cuestionario():
     print("------- CUESTIONARIO PA' SABER QUE AUTO TE CONVIENE Y NO PAGUI' DE MAS -------")
 
@@ -40,76 +47,35 @@ def mostrar_cuestionario():
             print("Error: Por favor ingresa un numero valido.")
     return pasajeros, terreno, dias
             
-def evaluar_opcion(pasajeros, terreno, dias):
-    vehiculo_elegido = None
-
-    match terreno:
-        case "1":
-            print("Modelos Auto/Suv: ")
-            print("1. Toyota Yaris ($35.000/dia - Traccion 4x2)")
-            print("2. Toyota RAV4 ($45.000/dia - Traccion AWD)")
-            while True:
-                eleccion = input("Elige el modelo que prefieras: ")
-                if eleccion == "1":
-                    vehiculo_elegido = AutoSuv("Toyota", "Yaris", 35000, 5, "4x2", "Ciudad", 18.5)
-                    break
-                elif eleccion == "2":
-                    vehiculo_elegido = AutoSuv("Toyota", "RAV4", 45000, 5, "AWD", "Ciudad", 14.0)
-                    break
-                print("Error: Opcion no valida.")
-        case "2":
-            print("Modelos todo terreno: ")
-            print("1. Toyota Hilux ($55.000/día - 4x4)")
-            print("2. Ford Ranger ($58.000/día - 4x4)")
-            while True:
-                eleccion = input("Elige el modelo que prefieras: ")
-                if eleccion == "1":
-                    vehiculo_elegido = Camioneta("Toyota", "Hilux", 55000, 5, "4x4", "Nieve/Playa", 12.0)
-                    break
-                elif eleccion == "2":
-                    vehiculo_elegido = Camioneta("Ford", "Ranger", 58000, 5, "4x4", "Nieve/Playa", 11.5)
-                    break
-                print("Error: Opcion no valida.")       
-        case "3":
-            print("Modelos de carga pesada: ")
-            print("1. Fuso Fighter ($90.000/día - 6x4)")
-            print("2. Mercedes Accelo ($75.000/día - 4x2)")
-            while True:
-                eleccion = input("Elige el modelo que prefieras: ")
-                if eleccion == "1":
-                    vehiculo_elegido = Camion("Fuso", "Fighter", 90000, 3, "6x4", "Trabajo Pesado", 5.0)
-                    break
-                elif eleccion == "2":
-                    vehiculo_elegido = Camion("Mercedes", "Accelo", 75000, 3, "4x2", "Trabajo Pesado", 7.0)
-                    break
-                print("Error: Opcion no valida.")
-        case "4":
-            print("Modelos de transporte de grupo: ")
-            print("1. Mercedes Sprinter (Capacidad: 15 pasajeros)")
-            print("2. Volkswagen Crafter (Capacidad: 20 pasajeros)")
-            while True:
-                eleccion = input("Elige el modelo que prefieras: ")
-                if eleccion == "1":
-                    vehiculo_elegido = Transporte("Mercedes", "Sprinter", 65000, 15, "4x2", "Grupo", 9.5)
-                    break
-                elif eleccion == "2":
-                    vehiculo_elegido = Transporte("Volkswagen", "Crafter", 75000, 20, "4x2", "Grupo", 10.0)
-                    break
-                print("Error: Opcion no valida.")
+def evaluar_opcion(pasajeros, terreno):
+    modelos_disponibles = flota[terreno]
+    print("------- Modelos disponibles para el terreno elegido -------")
+    for clave, vehiculo in modelos_disponibles.items():
+        print(f"{clave}.{vehiculo._marca} {vehiculo._modelo}")
+        print(f"  Motor: {vehiculo._motor}")
+        print(f"  Precio: ${vehiculo.get_precio_diario()} / dia")
+    while True:
+        eleccion = input("Elige el modelo que prefieras: ")
+        if eleccion in modelos_disponibles:
+            vehiculo_elegido = modelos_disponibles[eleccion]
+            break
+        else:
+            print("Error: Opcion no valida.")
     print(f"Has elegido un {vehiculo_elegido._marca} {vehiculo_elegido._modelo}")
     if pasajeros > vehiculo_elegido.get_capacidad_personas():
-        print(f"ALERTA: El vehiculo que elegiste cuenta con capacidad para {vehiculo_elegido.get_capacidad_personas()} personas.")
-        print(f"Pero indicaste que viajan {pasajeros} personas.")
+        print(f"ALERTA: El vehiculo que elegiste cuenta con capacidad para {vehiculo_elegido.get_capacidad_personas()} personas. Pero indicaste que viajan {pasajeros} personas.")
         print("Sugerencia: Reinicia el programa y elige una opcion de Modelos de transporte de grupo.")
+        return None
     else:
         print(f"Validacion completada: El vehiculo elegido cumple con las caracteristicas solicitadas.")
         return vehiculo_elegido
+    
 def generar_cotizacion(vehiculo, dias):
     vehiculo.mostrar_ficha_tecnica()
     total_a_pagar = vehiculo.calcular_cotizacion(dias)
     print(f"El total a pagar es: ${total_a_pagar}")
 
 pasajeros, terreno, dias = mostrar_cuestionario()
-vehiculo_listo = evaluar_opcion(pasajeros, terreno, dias)
+vehiculo_listo = evaluar_opcion(pasajeros, terreno)
 if vehiculo_listo is not None:
     generar_cotizacion(vehiculo_listo, dias)
